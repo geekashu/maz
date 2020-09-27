7ff7479e4b55f938b292f69874b7af79854f314d
Ashish Saini <geekashu@gmail.com>
2020-09-28 02:20:32 +0530

    Build

---
+++ frontend/build/precache-manifest.c3d0773bc0a79a20472e15e7460d76ad.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cefd7a2009ad5c9f0c7085fa91842f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "039406e42c3a3337f485",
+    "url": "/static/css/5.c53f186f.chunk.css"
+  },
+  {
+    "revision": "d75e74549ed932ce73ae",
+    "url": "/static/css/main.f500e6f3.chunk.css"
+  },
+  {
+    "revision": "0c7f16a2beaf50c33268",
+    "url": "/static/js/0.1a9888e8.chunk.js"
+  },
+  {
+    "revision": "acfb02d80341c5e28b9e",
+    "url": "/static/js/1.ba22ead6.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/1.ba22ead6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d611d4a0171fabb326",
+    "url": "/static/js/2.a1a72639.chunk.js"
+  },
+  {
+    "revision": "039406e42c3a3337f485",
+    "url": "/static/js/5.99d93a65.chunk.js"
+  },
+  {
+    "revision": "9d7f417610b1f1ed0f23bc30f22b0b5a",
+    "url": "/static/js/5.99d93a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981fbd3a66acaa2f6571",
+    "url": "/static/js/6.3bd18228.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/static/js/6.3bd18228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424b651e8abcce24da55",
+    "url": "/static/js/7.4b995cef.chunk.js"
+  },
+  {
+    "revision": "a06ef2cdf04e5e17552c",
+    "url": "/static/js/8.84883c17.chunk.js"
+  },
+  {
+    "revision": "1edb056656b5cc83960e",
+    "url": "/static/js/9.28aa20bf.chunk.js"
+  },
+  {
+    "revision": "d75e74549ed932ce73ae",
+    "url": "/static/js/main.544fc493.chunk.js"
+  },
+  {
+    "revision": "96f5203d73da40605a14",
+    "url": "/static/js/runtime-main.ddd24f2a.js"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  }
+]);
